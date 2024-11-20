e456a64c196283d64a2325d9bb7e344ce7659652
faizy <46767055+itsfaizy@users.noreply.github.com>
2024-11-20 08:24:12 -0400

    feat(support line-breaks for description field)

--- lab-HintUI/web/scripts.js
+++ lab-HintUI/web/scripts.js
@@ -1,13 +1,13 @@
 window.addEventListener('message', function(event) {
-	const {action, data} = event.data;
-	if (action == 'show') {
-		$("body").fadeOut(500);
-		$("#title").text(data.title);
-		$(".description").text(data.desc);
-		$("body").fadeIn(500);
-		$(".wrap").animate({left: '-3%'}, "slow");
-		$(".wrap").animate({left: '1%'}, "slow");
-	} else if (action == 'hide') {
-		$("body").fadeOut(500);
-	}
-})
+    const {action, data} = event.data;
+    if (action == 'show') {
+        $("body").fadeOut(500);
+        $("#title").text(data.title);
+        $(".description").html(data.desc.replace(/\n/g, '<br>'));
+        $("body").fadeIn(500);
+        $(".wrap").animate({left: '-3%'}, "slow");
+        $(".wrap").animate({left: '1%'}, "slow");
+    } else if (action == 'hide') {
+        $("body").fadeOut(500);
+    }
+});
